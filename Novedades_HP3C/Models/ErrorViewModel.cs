1d11505b2fa557ba5223169896d27a8ffa16d8f0
hrivas465 <hrivas0260@gmail.com>
2025-12-05 15:58:22 -0300

    inicio

---
+++ Novedades_HP3C/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Novedades_HP3C.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
