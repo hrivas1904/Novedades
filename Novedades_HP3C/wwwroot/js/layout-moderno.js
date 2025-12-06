3df0a6db00e4759eea41432cda94ba6a46cd2c4a
hrivas465 <hrivas0260@gmail.com>
2025-12-06 02:14:08 -0300

    nueva_vista

--- Novedades_HP3C/wwwroot/js/layout-moderno.js
+++ Novedades_HP3C/wwwroot/js/layout-moderno.js
@@ -1,3 +1,13 @@
-﻿$('#btnToggleSidebar').on('click', function () {
-    $('#sidebar').toggleClass('collapsed');
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const btn = document.getElementById("btnToggleSidebar");
+    const sidebar = document.getElementById("sidebarVertical");
+
+    if (!btn || !sidebar) return;
+
+    btn.addEventListener("click", function () {
+        sidebar.classList.toggle("collapsed");
+        sidebar.classList.toggle("show");
+    });
+
 });
