97fbaca25a98444303b257e98b5e2d3105a8053a
hrivas465 <hrivas0260@gmail.com>
2025-12-07 14:33:05 -0300

    CONFIRM

--- Novedades_HP3C/Controllers/PersonalController.cs
+++ Novedades_HP3C/Controllers/PersonalController.cs
@@ -1,12 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Novedades_HP3C.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
 
 namespace Novedades_HP3C.Controllers
 {
     public class PersonalController : Controller
     {
+        private readonly string cadenaSQL;
+
+        public PersonalController(IConfiguration config)
+        {
+            cadenaSQL = config.GetConnectionString("CadenaSQL");
+        }
+
         public IActionResult Personal()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListaEmpleados()
+        {
+            List<Empleados> lista = new List<Empleados>();
+
+            using (var conexion = new SqlConnection(cadenaSQL))
+            {
+                conexion.Open();
+                var cmd = new SqlCommand("SP_LISTA_EMPLEADOS", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        lista.Add(new Empleados
+                        {
+                            numeroLegajo = Convert.ToInt32(dr["numero_legajo"]),
+                            nombreCompleto = dr["nombre_completo"].ToString(),
+                            dni = dr["dni"].ToString(),
+                            cuil = dr["cuil"].ToString(),
+                            fechaIngreso = Convert.ToDateTime(dr["fecha_ingreso"]),
+                            antiguedad = Convert.ToDecimal(dr["antiguedad"]),
+                            area = dr["area"].ToString(),
+                            categoria = dr["categoria"].ToString(),
+                            servicio = dr["servicio"].ToString(),
+                            regimen = Convert.ToInt32(dr["regimen"]),
+                            horasDiarias = Convert.ToDecimal(dr["horas_diarias"]),
+                            convenio = dr["convenio"].ToString(),
+                            telefono = dr["telefono"].ToString()
+                        });
+                    }
+                }
+            }
+            return Json(new { data = lista });
         }
     }
 }
---
+++ Novedades_HP3C/Models/Empleados.cs
@@ -0,0 +1,27 @@
+﻿namespace Novedades_HP3C.Models
+{
+    public class Empleados
+    {
+        public int idEmpleado { get; set; }
+        public string cuil {  get; set; }
+        public string dni { get; set; }
+        public int numeroLegajo { get; set; }
+        public string nombreCompleto { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public int edad {  get; set; }
+        public DateTime fechaIngreso { get; set; }
+        public DateTime fechaFinPrueba { get; set; }
+        public DateTime fechaEgreso { get; set; }
+        public string area {  get; set; }
+        public string categoria { get; set; }
+        public string servicio {  get; set; }
+        public string correoElectronico { get; set; }
+        public string telefono { get; set; }
+        public string empleadoAntecesor { get; set; }
+        public decimal antiguedad { get; set; }
+        public int regimen { get; set; }
+        public decimal horasDiarias { get; set; }
+        public string convenio { get; set; }
+        public string rolSistema { get; set; }
+    }
+}
